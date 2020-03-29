1a4d30f27a7699e2e996bf4f8992423611bb7b5b
Chris Calo <ccalo@google.com>
2020-03-29 11:52:42 -0400

    Add example of reading a file from disk, set Node.js version to match Google App Engine

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { start } from "express-start";
 import server from "./server";
+import fs from "fs";
+import path from "path";
 
 console.log("index.js");
+console.log(`Node.js version: ${process.version}`);
 
-const main = !module.parent;
+if (!module.parent) {
+  main();
+}
 
-if (main) {
-  console.log("main!");
+function main() {
+  const pathToFile = path.join(__dirname, "file.txt");
+  console.log(`Reading contents from file: \n${pathToFile}`);
+  const contents = fs.readFileSync(pathToFile, { encoding: "utf-8" });
+  console.log(contents);
+  
+  console.log("main module, starting server!");
   start(server);
 }
